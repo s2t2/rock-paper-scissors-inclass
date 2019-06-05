fe87d117007a38146d67c3e3841445ce9fc1925e
Mike Rossetti <s2t2@users.noreply.github.com>
2019-06-05 19:46:22 -0400

    Determine winning option

--- game.py
+++ game.py
@@ -28,17 +28,26 @@ print("GENERATING...")
 print("COMPUTER CHOICE:", computer_choice)
 
 # DETERMINE THE WINNER
+#
+# rock beats scissors
+# paper beats rock
+# scissors beats paper
+# same selections is a tie
+#
 
-
-
-
-
-
-
-
-
-
-
-
+if user_choice == computer_choice:
+    print("TIE")
+elif user_choice == "rock" and computer_choice == "paper":
+    print("PAPER")
+elif user_choice == "rock" and computer_choice == "scissors":
+    print("ROCK")
+elif user_choice == "paper" and computer_choice == "rock":
+    print("PAPER")
+elif user_choice == "paper" and computer_choice == "scissors":
+    print("SCISSORS")
+elif user_choice == "scissors" and computer_choice == "rock":
+    print("ROCK")
+elif user_choice == "scissors" and computer_choice == "paper":
+    print("SCISSORS")
 
 # DISPLAY FINAL OUTPUTS / OUTCOMES
